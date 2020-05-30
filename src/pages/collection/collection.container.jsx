0122bec21f7f89c4072e0343bc97b49add811d6a
Andrew Dubovtsev <andrew.dubovtsev@viasat.com>
2020-05-30 13:46:45 +0100

    refactored the app to use a container pattern

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,17 @@
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {selectAreCollectionsLoaded} from '../../redux/shop/shop.selectors';
+import {compose} from 'redux';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import CollectionPage from './collection.component';
+
+const mapStateToProps = createStructuredSelector({
+   isLoading: state => !selectAreCollectionsLoaded(state)
+});
+
+const CollectionPageContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionPage);
+
+export default CollectionPageContainer;
